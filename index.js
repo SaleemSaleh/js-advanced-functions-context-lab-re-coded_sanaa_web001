2df4be2ce6681b836c083b1a2cbcd76dada5ed62
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-29 22:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,9 +26,72 @@ function createEmployeeRecord (arraySt){
   return {
       firstName : arraySt[0],
       familyName: arraySt[1],
-      payPerHour: arraySt[3],
+      title: arraySt[2],
       payPerHour: arraySt[3],
       timeInEvents : [],
       timeOutEvents:[]
   }
+}
+
+
+function createEmployeeRecords (arraySt){
+  return arraySt.map (x => createEmployeeRecord(x));
+}
+
+function createTimeInEvent (date){
+  let arrDa = date.split(" ") ;
+  let ob = {
+    type:"TimeIn" ,
+    hour: parseInt(arrDa[1]),
+    date:arrDa[0]
+  };
+  this.timeInEvents.push(ob);
+  return this;
+}
+
+function createTimeOutEvent ( date){
+  let arrDa = date.split(" ") ;
+  let ob = {
+    type:"TimeOut" ,
+    hour: parseInt(arrDa[1]),
+    date:arrDa[0]
+  };
+  this.timeOutEvents.push(ob);
+  return this;
+}
+
+function hoursWorkedOnDate (date){
+  let arrDa = date.split(" ") ;
+  for (let i = 0 ; i < this.timeInEvents.length ;i++ ) {
+    if(arrDa[0] === this.timeInEvents[i].date){
+      return (this.timeOutEvents[i].hour - this.timeInEvents[i].hour )/ 100;
+    }
+  }
+}
+
+function wagesEarnedOnDate (date){
+  return hoursWorkedOnDate.bind(this,date) * this.payPerHour ;
+}
+
+function allWagesFor (){
+  let hour = 0;
+  for (let i = 0 ; i < this.timeInEvents.length ; i++ ) {
+    hour += ((this.timeOutEvents[i].hour - this.timeInEvents[i].hour) / 100 );
+  }
+  return hour * this.payPerHour ;
+}
+
+function findEmployeeByFirstName (srcArray,firstName ){
+  let s= srcArray.find((x) => x.firstName === firstName);
+  return s;
+}
+
+function calculatePayroll (srcArray ){
+  let sum = 0 ;
+  for (let i = 0 ; i < srcArray.length ; i++ ) {
+    for (let j = 0 ; j < srcArray[i].timeInEvents.length ; j++ ){
+      sum += wagesEarnedOnDate(srcArray[i],srcArray[i].timeInEvents[j].date) ;
+    }
+  }
+  return sum ;
 }
